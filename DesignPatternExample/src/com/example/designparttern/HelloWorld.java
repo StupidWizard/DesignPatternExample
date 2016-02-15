bd4a04550453bc0b1f06edc6ecba1138854991ea
thantieuhodo <toinguyenkhac@gmail.com>
2016-02-15 15:36:39 +0900

    Change HelloWorld

--- DesignPatternExample/src/com/example/designparttern/HelloWorld.java
+++ DesignPatternExample/src/com/example/designparttern/HelloWorld.java
@@ -1,11 +1,19 @@
 package com.example.designparttern;
 
+import java.util.Hashtable;
+
 /**
  * This stupid code is created by thantieuhodo on 10/9/15.
  */
 public class HelloWorld {
 
+
+
     public static void main(String args[]) {
         System.out.println("Hello World");
+
+        Hashtable ht = new Hashtable();
+        ht.keys();
+
     }
 }
